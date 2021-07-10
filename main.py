553ebe08ce80b0554bae9820e35c2d5f7a265dd7
Nishant Chaudhary <nishantchy1234@gmail.com>
2021-07-10 19:42:30 +0545

    add main.py file

---
+++ main.py
@@ -0,0 +1,17 @@
+mylist = ["banana", "apple", "pineapple"]
+print(mylist)
+item = mylist[2]
+print(item)
+if "banana" in mylist:
+    print("yes")
+else:
+    print("no")
+mylist.append("lemon")
+print(mylist)
+mylist.insert(2, "grapes")
+print(mylist)
+
+
+# /item = [0] * 5
+#  print(item)
+
